3a666f12eb2813e2999e21ab1cd83b868c702aa4
adrianna <acgalletta@gmail.com>
2019-06-04 07:26:04 -0700

    first commit

---
+++ Project/P1/huffman_code.py
@@ -0,0 +1,45 @@
+import sys
+
+def char_frequency(string):
+
+    char_freq = {}
+    for chr in range(len(string)):
+        if chr
+
+
+
+def huffman_encoding(data):
+    pass
+
+
+
+
+def huffman_decoding(data,tree):
+    pass
+
+
+
+def char_frequecny_cnt():
+
+
+
+
+if __name__ == "__main__":
+    codes = {}
+
+    a_great_sentence = "The bird is the word"
+
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
+
+    

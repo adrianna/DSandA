ae65f174c507c609105d59e3ea3b8bfe5b244d1c
adrianna <acgalletta@gmail.com>
2019-06-06 21:47:48 -0700

    BST find first and last indices encompassing the repeating target

---
+++ PyLibrary/BinarySearch/firstAndlastIndex.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Problem statement
+# 
+# Given a sorted array that may have duplicate values, use *binary search* to find the **first** and **last**
+# indexes of a given value.
+# 
+# For example, if you have the array `[0, 1, 2, 2, 3, 3, 3, 4, 5, 6]` and the given value is `3`, the answer
+# will be `[4, 6]` (because the value `3` occurs first at index `4` and last at index `6` in the array).
+# 
+# The expected complexity of the problem is $O(log(n))$.
+
+# Combining two BST searches for the respective indices
+def first_and_last_index(arr, number):
+    # search first occurence
+    first_index = find_start_index(arr, number, 0, len(arr) - 1)
+    
+    # search last occurence
+    last_index =  find_end_index(arr, number, 0, len(arr) - 1)
+    return [first_index, last_index]
+
+
+# Use BST to find the first index
+def find_start_index(arr, number, start_index, end_index):
+    # binary search solution to search for the first index of the array
+    if start_index > end_index:
+        return -1
+
+    mid_index = start_index + (end_index - start_index)//2
+
+    if arr[mid_index] == number:
+        current_start_pos = find_start_index(arr, number, start_index, mid_index - 1)
+        if current_start_pos != -1:
+            start_pos = current_start_pos
+        else:
+            start_pos = mid_index
+        return start_pos
+
+    elif arr[mid_index] < number:
+        return find_start_index(arr, number, mid_index + 1, end_index)
+    else:
+        return find_start_index(arr, number, start_index, mid_index - 1)
+
+# Use BST to find second index
+def find_end_index(arr, number, start_index, end_index):
+    # binary search solution to search for the last index of the array
+    if start_index > end_index:
+        return  -1
+
+    mid_index = start_index + (end_index - start_index)//2
+
+    if arr[mid_index] == number:
+        current_end_pos = find_end_index(arr, number, mid_index + 1, end_index)
+        if current_end_pos != -1:
+            end_pos = current_end_pos
+        else:
+            end_pos = mid_index
+        return end_pos
+    elif arr[mid_index] < number:
+        return find_end_index(arr, number, mid_index + 1, end_index)
+    else:
+        return find_end_index(arr, number, start_index, mid_index - 1)
+
+
+# Below are several different test cases you can use to check your solution.
+
+# In[2]:
+
+
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    solution = test_case[2]
+    output = first_and_last_index(input_list, number)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+# In[ ]:
+
+
+input_list = [1]
+number = 1
+solution = [0, 0]
+test_case_1 = [input_list, number, solution]
+test_function(test_case_1)
+
+
+# In[ ]:
+
+
+input_list = [0, 1, 2, 3, 3, 3, 3, 4, 5, 6]
+number = 3
+solution = [3, 6]
+test_case_2 = [input_list, number, solution]
+test_function(test_case_2)
+
+
+# In[ ]:
+
+
+input_list = [0, 1, 2, 3, 4, 5]
+number = 5
+solution = [5, 5]
+test_case_3 = [input_list, number, solution]
+test_function(test_case_3)
+
+
+# In[ ]:
+
+
+input_list = [0, 1, 2, 3, 4, 5]
+number = 6
+solution = [-1, -1]
+test_case_4 = [input_list, number, solution]
+test_function(test_case_4)
+

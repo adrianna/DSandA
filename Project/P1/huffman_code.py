e0aabfa4829803ccdba76884971f49bea983d696
adrianna <acgalletta@gmail.com>
2019-06-11 18:48:12 -0700

    modifications

--- Project/P1/huffman_code.py
+++ Project/P1/huffman_code.py
@@ -2,43 +2,36 @@ from DataStructures import *
 import heapq
 
 class HeapNode:
-    def __init__(self, char, freq):
+    def __init__(self, char, frequency):
         self.char = ''
-        self.freq = 0
+        self.frequency = 0
         self.right = None
         self.left = None
 
 class HuffmanCode:
-        
-    def char_frequency(string):
-        
-        # Create Hash Counter for character frequency
-        char_freq = {}
-        for chr in string:
-            if ' ' not in chr:
-                if chr not in char_freq.keys():
-                    char_freq[chr] = 1
-                else:
-                    char_freq[chr] += 1
-                    
-                    # Convert to tuples, whose values are *immutable*
-        char_freq_tuple = list()
-        for chr, count in char_freq.items():
-        char_freq_tuple.append((count, chr))
-        
-        ## Note: Do we skip spaces?
-        ##       Are Character frequency case-sensitive?
-        return sorted(char_freq_tuple)
 
-    def createHuffTree(cf_list):
+    
+    def createFreqDict(self,string):
+
+        # Create Hash Counter for character frequency
+        freq_dict = {}
+        for char in string:
+            if ' ' not in char:
+                if char not in freq_dict.keys():
+                    freq_dict[char] = 1
+                else:
+                    freq_dict[char] += 1
+                    
+        return freq_dict
+
+    def createHuffTree(freq_dict):
         
         huff_tree = heapq()
-        for chr, freq in cf_list:
-            print("chr: {}, freq: {}".format(chr, freq))
-            
-            char_node = HeapNode(chr, freq)
-            heapq.heappush(huff_tree, char_node)
-            
+        for key in freq_dict.keys():
+            character_node = HeapNode(key, frequency)
+            heapq.heappush(huff_tree, character_node)
+
+        print(huff_tree)
             
     
 
@@ -63,10 +56,18 @@ if __name__ == "__main__":
 
     a_great_sentence = "The bird is the word"
     print(a_great_sentence)
-    print (char_frequency(a_great_sentence))
-    cf_list = char_frequency(a_great_sentence)
-    createHuffTree(cf_list)
-#    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+
+    h = HuffmanCode()
+    print(h.createFreqDict(a_great_sentence))
+
+#    print (char_frequency(a_great_sentence))
+#    cf_list = char_frequency(a_great_sentence)
+#    createHuffTree(cf_list)
+
+
+
+
+    #    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
 #    print ("The content of the data is: {}\n".format(a_great_sentence))
 
 #    encoded_data, tree = huffman_encoding(a_great_sentence)

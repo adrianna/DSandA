b851b302efbeb92db3f9bb6e5f0b70d6fdc45cc6
adrianna <acgalletta@gmail.com>
2019-06-16 12:45:39 -0700

    completed

--- Project/P2/unsorted_integer_array.py
+++ Project/P2/unsorted_integer_array.py
@@ -5,7 +5,16 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
-   pass
+    min_int = 99
+    max_int = -1
+    # Traverse the array comparing the elements to the stored integer
+    # Replace the stored integer min or max with the smaller or larger of
+    # the two numbers, respectively
+    for num in range(len(ints)):
+        min_int = min(min_int, num)
+        max_int = max(max_int, num)
+        
+    return (min_int, max_int)
 
 ## Example Test Case of Ten Integers
 import random

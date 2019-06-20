e524dc5d6bbc42f6ec1121b384b56429c0b46b1d
adrianna <acgalletta@gmail.com>
2019-06-19 22:23:29 -0700

    added quicksort routines

--- Project/P2/search_rotated_array.py
+++ Project/P2/search_rotated_array.py
@@ -7,7 +7,40 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
-   pass
+    sort_all(input_list, number, len(items) - 1)
+    
+
+
+def sort_a_little_bit(items):
+    left_index = 0
+    pivot_index = len(items) - 1
+    pivot_value = items[pivot_index]
+
+    while (pivot_index != left_index):
+
+        item = items[left_index]
+
+        if item <= pivot_value:
+            left_index += 1
+            continue
+
+        items[left_index] = items[pivot_index - 1]
+        items[pivot_index - 1] = pivot_value
+        items[pivot_index] = item
+        pivot_index -= 1
+
+
+def sort_all(items, begin_index, end_index):
+    if end_index <= begin_index:
+        return
+    
+    pivot_index = sort_a_little_bit(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index - 1)
+    sort_all(items, pivot_index + 1, end_index)
+    
+def quicksort(items):
+   
+
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):

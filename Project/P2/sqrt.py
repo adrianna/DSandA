361c3d76b90fba756c0ba82c11d9fc7dc888300e
adrianna <acgalletta@gmail.com>
2019-06-21 23:39:06 -0700

    Does not pass for sqrt(27)

--- Project/P2/sqrt.py
+++ Project/P2/sqrt.py
@@ -18,33 +18,43 @@ def sqrt(number):
     
     ref_sq = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256,
               289, 324, 361, 400]
-    
-    
 
-    pass
+    start_pt = 0
+    end_pt = len(ref_sq)
+    mid_pt = (end_pt - start_pt) // 2;
+    base = binSearchBase(ref_sq, number, mid_pt)
+
+    return base
 
 
     
 def binSearchBase(input_list, number, idx):
+
     start_pt = 0
-    end_pt = len(input_list)
-    mid_pt = len(ref_sq) // 2;
-    
-    if number == ref_sq[mid_pt]:
-        base = mid_pt + 1
+    mid_pt = idx
+    base = 0
+    if number == 0:
         return base
-    elif number < ref_sq[mid_pt]:
+    if number == input_list[mid_pt]:
+        base = mid_pt + 1
+#        print("number equals il[mid]: {}".format(base))
+        return base
+    elif input_list[mid_pt] < number & number < input_list[mid_pt+1]:
+        base = mid_pt + 1  # Base numbers = 1+idx
+#        print("il[mid] <number < il[mid+1]: {}".format(base))
+        return base
+    elif input_list[mid_pt] > number:
+        # recalculate the mid_pt
         end_pt = mid_pt
         mid_pt = (end_pt - start_pt)//2
-        binSearchBase(input_list, number, mid_pt)
-    elif number > ref_sq[mid_pt]:
-        start = mid_pt + 1
-        mid_pt = (end_pt - start_pt)//2
-        binSearchBase(input_list, number, mid_pt)
+        return binSearchBase(input_list, number, mid_pt)
 
 
 
     
+print("Sqrt(4): {}".format(sqrt(4)))
+print("Sqrt(5): {}".format(sqrt(5)))
+print("Sqrt(27): {}".format(sqrt(5)))
 
 
 

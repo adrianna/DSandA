996f9233658f20fcde0e454da01b13d7049524dc
adrianna <acgalletta@gmail.com>
2019-06-22 08:42:02 -0700

    Only does sqrt(n), n< 400

--- Project/P2/sqrt.py
+++ Project/P2/sqrt.py
@@ -19,42 +19,58 @@ def sqrt(number):
     ref_sq = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256,
               289, 324, 361, 400]
 
-    start_pt = 0
-    end_pt = len(ref_sq)
-    mid_pt = (end_pt - start_pt) // 2;
-    base = binSearchBase(ref_sq, number, mid_pt)
 
+    start_pt = 0
+    end_pt = len(ref_sq) - 1
+    mid_pt = (end_pt - start_pt) // 2;
+  #  print("length of ref_sq: {}".format(end_pt))
+    base = binSearchBase(ref_sq, number, mid_pt, start_pt, end_pt)
+  #  print("sqrt(): {}".format(base))
     return base
 
 
     
-def binSearchBase(input_list, number, idx):
+def binSearchBase(input_list, number, idx, start_pt, end_pt):
 
-    start_pt = 0
     mid_pt = idx
     base = 0
     if number == 0:
         return base
     if number == input_list[mid_pt]:
         base = mid_pt + 1
-#        print("number equals il[mid]: {}".format(base))
+   #     print("number equals il[mid]: {}".format(base))
         return base
-    elif input_list[mid_pt] < number & number < input_list[mid_pt+1]:
-        base = mid_pt + 1  # Base numbers = 1+idx
-#        print("il[mid] <number < il[mid+1]: {}".format(base))
-        return base
+    elif input_list[mid_pt] < number:
+        
+        if number < input_list[mid_pt+1]:
+            base = mid_pt + 1  # Base numbers = 1+idx
+    #        print("il[mid] <number < il[mid+1]: {}".format(base))
+    #        print("\t{}".format(input_list[mid_pt]))
+    #        print("\t{}".format(number))
+    #        print("\t{}".format(base))
+            
+            return base
+        else:
+            start_pt = mid_pt
+            mid_pt = start_pt + (end_pt - start_pt)//2
+            return binSearchBase(input_list, number, mid_pt, start_pt, end_pt)
+            
     elif input_list[mid_pt] > number:
         # recalculate the mid_pt
+    #    print(input_list[mid_pt])
+    #    print(number)
+    #    print("\trecurse... at midpt: {}",format(mid_pt))
         end_pt = mid_pt
-        mid_pt = (end_pt - start_pt)//2
-        return binSearchBase(input_list, number, mid_pt)
+        mid_pt = start_pt + (end_pt - start_pt)//2
+
+        return binSearchBase(input_list, number, mid_pt, start_pt, end_pt)
 
 
 
     
-print("Sqrt(4): {}".format(sqrt(4)))
-print("Sqrt(5): {}".format(sqrt(5)))
-print("Sqrt(27): {}".format(sqrt(5)))
+#print("Sqrt(4): {}".format(sqrt(4)))
+#print("Sqrt(5): {}".format(sqrt(5)))
+#print("Sqrt(27): {}".format(sqrt(5)))
 
 
 
@@ -63,3 +79,5 @@ print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+
+print ("Pass" if  (22 == sqrt(500)) else "Fail")

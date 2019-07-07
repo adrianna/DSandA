fef96a6feab5e6186c219738eebeedf27238bcf0
adrianna <acgalletta@gmail.com>
2019-07-06 19:12:55 -0700

    issue with test case

--- Project/P2/search_rotated_array.py
+++ Project/P2/search_rotated_array.py
@@ -26,21 +26,52 @@ def rotated_array_search(input_list, number):
        int: Index or -1
     """
 
+    start_index = 0
+    end_index = len(input_list) - 1
+
+    print("[[rotated_array_search]], target number: {}".format(number))
+    while start_index <= end_index:
+        print("\tstart_index: {}, end_index: {}".format(start_index, end_index))
+        mid_index = (start_index + end_index) // 2
+        mid_element = input_list[mid_index]
+        
+        if number == mid_element:
+            print("\t\tfound!")
+            return mid_index
+
+        elif number < mid_element:
+            
+            print("number < mid_element: {}".format(mid_element))
+            if withinBoundary(input_list[ mid_index + 1 : end_index], number):
+            #            if number <= input_list[end_index] and number >= input_list[mid_index+1]:
+                start_index = mid_index + 1
+            else:
+                end_index = mid_index - 1
+
+        else:
+            
+            print("number > mid_element: {}".format(mid_element))
+            print("passing, start_index: {}, mid_index: {}".format(start_index, mid_index))
+            #            if input_list[start_index] <= number and number >= input_list[mid_index-1]:
+            if withinBoundary(input_list[start_index : mid_index - 1], number):
+                end_index = mid_index - 1 
+            else:    
+                start_index = mid_index + 1
     
-# Rotate Array by Pivot, x
-def rotateLeft(input_list, x):
-    temp = input_list[0]
-    end = len(input_list) - 1
-    for dgt in range(x):
-        #print("Currently: {}".format(input_list[dgt]))
-        #print("At dgt+x: {}, number: {}".format(dgt+x,input_list[dgt+x]) )
-        input_list[dgt], input_list[dgt+x]  = input_list[dgt+x], input_list[dgt]
-        #print("Now: {}".format(input_list[dgt]))
-    input_list[x] = temp
+    return -1
 
-    #input_list[end] = temp
 
-    return input_list
+def withinBoundary(input_list, number):
+
+    start_index = 0
+    end_index = len(input_list) - 1
+
+    print("[[withinBoundary]] start: {}, end: {}".format(start_index, end_index))
+    if input_list[start_index] <= number and number <= input_list[end_index]:
+        return True
+
+    return False
+    
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
@@ -51,27 +82,17 @@ def linear_search(input_list, number):
 def test_function(test_case):
     input_list = test_case[0]
     number = test_case[1]
+    print("[[test_function]]")
     if linear_search(input_list, number) == rotated_array_search(input_list, number):
         print("Pass")
     else:
         print("Fail")
 
-
-input_list = [6, 7, 8, 9, 10, 1, 2, 3, 4]
-print("Before: {}".format(input_list))
-
-print("pivot at 4")
-print("After: {}".format(rotateLeft(input_list, 4)))
-print("pivot at 3")
-input_list = [6, 7, 8, 9, 10, 1, 2, 3, 4]
-print("After: {}".format(rotateLeft(input_list, 3)))
-print("pivot at 2")
-input_list = [6, 7, 8, 9, 10, 1, 2, 3, 4]
-print("After: {}".format(rotateLeft(input_list, 2)))
-
         
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+#test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+#test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+#test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+#test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+
+#test_function([[7,8,9,1,2,3,4,5,6]], 9])

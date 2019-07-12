12a268f13edb2a7bd2edfb848e550f8b62ea0afc
adrianna <acgalletta@gmail.com>
2019-07-11 18:06:28 -0700

    working

--- Project/P2/http_router_using_tries.py
+++ Project/P2/http_router_using_tries.py
@@ -18,8 +18,6 @@ class RouteTrieNode:
 
         if self.paths.get(handler) is None:
             
-            if handler == "/":
-                self.handler = "root handler"
             self.paths[handler] = RouteTrieNode()
 
             
@@ -29,23 +27,20 @@ class RouteTrie:
     def __init__(self, handler):
         # Initialize the trie with an root node and a handler, this is the root path or home page node
         self.root = RouteTrieNode(handler)
-
+        self.handler = handler
         
     def insert(self, path_list, handler):
         # Similar to our previous example you will want to recursively add nodes
         # Make sure you assign the handler to only the leaf (deepest) node of this path
 
         current_path = self.root
-        if handler == "/" or handler == "root handler":
-            current_path.insert(handler)
-        else:
+        
+        for p in path_list:
+            current_path.insert(p)            
+            current_path = current_path.paths[p]
             
-            for p in path_list:
-                current_path.insert(p)            
-                current_path = current_path.paths[p]
-                
-            current_path.is_handler = True
-            current_path.handler = handler
+        current_path.is_handler = True
+        current_path.handler = handler
         
     def find(self, path_list):
         # Starting at the root, navigate the Trie to find a match for this path
@@ -56,12 +51,13 @@ class RouteTrie:
         if current_node.paths is None:
             return "No match"
 
-        
         for p in path_list:
-            if current_node.paths[p]:
-                if current_node.handler == current_node.convertRootHandler(p):
-                    return current_node.handler
+            if current_node.paths.get(p):
                 current_node = current_node.paths[p]
+            else:
+                return None
+        return current_node.handler
+            
             
         
             
@@ -71,7 +67,7 @@ class Router:
         # Create a new RouteTrie for holding our routes
         # You could also add a handler for 404 page not found responses as well!
         self.routeTrie = RouteTrie(handler_list)
-
+        self.handler_list = handler_list
  
 
     def add_handler(self, path, handler):
@@ -79,11 +75,9 @@ class Router:
         # You will need to split the path and pass the pass parts
         # as a list to the RouteTrie
 
-        if handler == "/":
-            handler_list.append(handler)
-        else:
-            handler_list = self.split_path(path)
-            current_path = self.routeTrie
+        
+        handler_list = self.split_path(path)
+        current_path = self.routeTrie
         
         current_path.insert(handler_list, handler)
             
@@ -96,8 +90,13 @@ class Router:
         # e.g. /about and /about/ both return the /about handler
 
         current_path = self.routeTrie
-
-        found_handler = current_path.find(handler)
+        if handler == "/":
+            return current_path.handler
+        
+        
+        path_list = self.split_path(handler)
+        
+        found_handler = current_path.find(path_list)
         
         if found_handler:
             return found_handler
@@ -109,16 +108,15 @@ class Router:
         # both the add_handler and loopup functions,
         # so it should be placed in a function here
 
-        if handler == "/":
-            return [handler]
-        if handler:
-            hlist = [handler[0] ] + handler[1:].split("/")
-            print(hlist)
 
+        if handler:
+            hlist = []            
+            for item in handler.split("/"):
+                if item:
+                    hlist.append(item)
+                        
             return hlist
         
-        
-
 
         
 # Here are some test cases and expected outputs you can use to test your implementation
@@ -130,8 +128,19 @@ router.add_handler("/home/about", "about handler")  # add a route
 
 # some lookups with the expected output
 print(router.lookup("/")) # should print 'root handler'
-#print(router.lookup("/home")) # should print 'not found handler' or None if you did not implement one
-#print(router.lookup("/home/about")) # should print 'about handler'
-#print(router.lookup("/home/about/")) # should print 'about handler' or None if you did not handle trailing slashes
-#print(router.lookup("/home/about/me")) # should print 'not found handler' or None if you did not implement one
+print(router.lookup("/home")) # should print 'not found handler' or None if you did not implement one
+print(router.lookup("/home/about")) # should print 'about handler'
+print(router.lookup("/home/about/")) # should print 'about handler' or None if you did not handle trailing slashes
+print(router.lookup("/home/about/me")) # should print 'not found handler' or None if you did not implement one
+
+router.add_handler("/usr/bin", "bin handler")
+print(router.lookup("/usr/bin"))
+
+router.add_handler("/usr/include", "include handler")
+print(router.lookup("/usr/include"))
+
+router.add_handler("/usr/sbin", "sbin handler")
+print(router.lookup("/usr/sbin"))
+
+
 

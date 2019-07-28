0089d305b0cfe8e611bd907ef49ca30b65bf4b02
adrianna <acgalletta@gmail.com>
2019-07-27 20:28:04 -0700

    a_star alg

--- Project/P3/student_code.py
+++ Project/P3/student_code.py
@@ -22,70 +22,149 @@
 ## Repeat!
 
 
-# Route Planner
-
-class GraphNode(object):
-    def __init__(self, val):
-        self.value = val
-        self.children = []
-        
-    def add_child(self,new_node):
-        self.children.append(new_node)
-    
-    def remove_child(self,del_node):
-        if del_node in self.children:
-            self.children.remove(del_node)
-
-class Graph(object):
-    def __init__(self,node_list):
-        self.nodes = node_list
-        
-    def add_edge(self,node1,node2):
-        if(node1 in self.nodes and node2 in self.nodes):
-            node1.add_child(node2)
-            node2.add_child(node1)
-            
-    def remove_edge(self,node1,node2):
-        if(node1 in self.nodes and node2 in self.nodes):
-            node1.remove_child(node2)
-            node2.remove_child(node1)
-
-
-## Create Graph Tree
-
 # These Map objects have two properties you will want to use to implement
 # A* search: intersections and roads
 
 # Intersections
-
 # The intersections are represented as a dictionary.
 
 # In this example, there are 10 intersections, each identified by an x,y
 # coordinate. The coordinates are listed below. You can hover over each dot
 # in the map above to see the intersection number.
 
+from helpers import Map, load_map, show_map
+from student_code import shortest_path
+
+import math
+import heapq as h
+
+map_10 = load_map('map-10.pickle')
+show_map(map_10)
+
+#map_10.intersections
+#map_10.roads
+
+
+
 ## Find shortest path
 def shortest_path(M,start,goal):
-   # Initialize open and closed lists
-   # Make the start vertex current
-   # Calculate heuristic distance of start vertex to destination (h)
-   # Calculate f value for start vertex (f= g+h, where g= 0)
-   # While current vertex is not the destination
-   #    FOR each vertex adjacent to current
-   #       IF vertex not in closed list and not in open list THEN
-   #            Add vertex to open list
-   #            Calculate distance from start (g)
-   #            Calculate heuristic distance to destination (h)
-   #            Calculate f value (f = g+h)
-   #            IF new f value < existing f value or there is no existing f value THEN
-   #               Update f value
-   #               Set parent to be current vertex
-   #            END IF
-   #       END IF
-   # NEXT adjacent vertex
-   # Add current vertex to closed list
-   # Remove vertex with lowest f value from open list and make it current
-   # END WHILE 
     
-    print("shortest path called")
-    return
+   # Initialize open and closed lists
+   open_path = []
+   closed_path = []
+
+   initialize(g_score, M)
+   initialize(f_score, M)
+
+   # Make the start vertex current
+   current_node = start
+   open_path.append(current_node)
+    
+   
+   # Calculate heuristic distance of start vertex to destination (h)
+   h_score[start] = hScore(M.intersections[start], M.intersections[goal])
+   
+   # Calculate f value for start vertex (f= g+h, where g= 0)
+   f_score[start] = fScore(h_score[start])
+
+   frontier = []
+   h.heapify(frontier, (current_node, f_score[start]))
+   
+   while open_path:
+
+    # Take node from lowest path
+    current_node = h.pop()
+    if current_node = goal:
+        return reconstruct_path(cameFrom, current_node);
+    
+    calculatePathCost(frontier, M.roads[current_node])
+       
+    open_path.remove(current_node)
+    closed_path.append(current_node)
+    
+    for neighbor in M.roads[current_node]:
+        if neighbor in closed_path:
+            g_score_tmp[neighbor] = g_score[current_node] + dist(M.intersections[current_node], M.intersections[neighbor])
+        if neighbor not in open_path:
+            open_path.append(neighbor)
+        elif g_score_tmp[neighbor] > g_score[neighbor]:
+            cameFrom[neighbor] = current_node
+            h_score[neighbor] = hScore(M.intersections[current_node], M.intersections[neighbor], "pythagorus")
+            g_score[neighbor] = g_score_tmp[neighbor]
+            f_score[neighbor] = g_score[neighbor] + h_score[neigbor]
+            
+
+            
+
+
+
+   
+    
+#    print("shortest path called")
+#    return
+
+
+
+## Calculate Path Cost
+
+def calculatePathCost(f, road_list):
+    
+    pass
+
+
+## Reconstruct Path
+def reconstruct_path(cameFrom, current):
+    total_path = current
+    while current in cameFrom.keys:
+        current = cameFrom[current]
+        total_path.prepend(current)
+
+    return total_path
+
+## Initalize Scores
+def initialize(cost_list, map):
+    node_count = len(map.keys())
+
+    for idx in range(node_count):
+        cost_list[idx] = math.inf
+
+        
+
+## Cost Path Calculation
+
+# h() Estimated Cost Path
+def hScore(xy_start, xy_end, cost_path):
+    start_x = start_xy[0]
+    start_y = start_xy[1]
+
+    end_x = goal_xy[0]
+    end_y = goal_xy[1]
+
+    dx = end_x - start_x
+    dy = end_y - start_y
+    
+    if cost_path == "manhattan":
+        return manhattan_distance(dx,dy)
+    
+    else: # "pythagoras"
+        return hypothenuse(dx,dy)
+   
+# f() Total Cost Path    
+def fScore(gDist = 0, hDist):
+    return gDist + hDist
+
+def dist(xy_start, xy_end, cost_path):
+    return hScore(xy_start, xy_end, cost_path)
+    
+# Pythagorus Method
+def hypothenuse(x,y):
+    return math.hypot(dx, dy)
+
+# Manhattan Distance
+def manhattan_distance(x,y):
+    return x + y
+
+    
+
+
+        

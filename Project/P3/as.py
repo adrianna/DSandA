4973da7d7e891509631142018b0cedc2cfb1f678
adrianna <acgalletta@gmail.com>
2019-08-06 22:51:16 -0700

    update

--- Project/P3/as.py
+++ Project/P3/as.py
@@ -28,6 +28,7 @@ class map_10(object):
                                7: [0.46247219371675075, 0.6258061621642713],
                                8: [0.11622158839385677, 0.11236327488812581],
                                9: [0.1285377678230034, 0.3285840695698353] }
+        
         self.roads = [[7, 6, 5],
                       [4, 3, 2],
                       [4, 3, 1],
@@ -73,20 +74,58 @@ def shortest_path(graph, start, goal):
 
   current = start
 
-  path_cost = 0
-  while current != goal:
+  while open_path:
 
+    # current = Pop from open_list heap to retrieve lowest f-score node
+    # Append to neighbor to closed path (?)     
 
     
     for neighbor in graph.roads[current]:
-
-      # if neighbor not in open path 
-      if closed_path.get(neighbor) is None:
-        
-        # calculate f score of neighbor
+      
+      if neighbor not in open_path:
+        # calculate f,g,h score of neighbor
         g_score = dist_between(graph.intersections[current], graph.intersections[neighbor])
         h_score = dist_between(graph.intersections[neighbor], graph.intersections[goal])
         f_score = g_score + h_score
+
+      elif neighbor in open_path:
+        # compare the g-value recently computed against the existing
+        if g_score <= g(neighbor):
+          g(neighbor) = g_score
+          f(neighbor) = f_score
+          neighbor.parent = current
+
+
+          #   #   # Append to neighbor to closed path (?)
+
+  if current = goal
+  return path
+
+ return "Not found!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################################################################
+
+
+
+      
+      
+      if closed_path.get(neighbor) is None:
+        
+        # calculate f score of neighbor
 
         
         #open_path.append(neighbor)
@@ -94,11 +133,13 @@ def shortest_path(graph, start, goal):
         
     # for loop is done, compare f score against neighbor's f_score
     f_min, g_min, neighbor = open_path.get()
+
     # create new Node = neighbor with f_min
 
     # advance current to neighbor with f_min
     # track the neighbor.parent = current
     # (g_score) path_cost = g_min
+    
     # store current (neighbor with fmin) into closed_path
     
     

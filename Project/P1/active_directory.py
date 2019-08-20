bed01b1fdfd6ad008b6d681b71639f6fc3215a91
adrianna <acgalletta@gmail.com>
2019-08-20 11:53:36 -0700

    cleaned up code

--- Project/P1/active_directory.py
+++ Project/P1/active_directory.py
@@ -6,22 +6,19 @@
 #
 ##################################
 
-
+debug = 0
 class Group(object):
     def __init__(self, _name):
         self.name = _name
         self.groups = []      # list of class Groups
         self.users = []       # list of string user
-        # print("[[Group_init_]] name = {}".format(self.name))
-        # print("[[Group_init_]] groups = {}".format(self.groups))
-        # print("[[Group_init_]] users = {}".format(self.users))
 
     def add_group(self, group):
-        print("[[add_group]]: {}".format(group))
+        if debug: print("[[add_group]]: {}".format(group))
         self.groups.append(group)
 
     def add_user(self,user):
-        print("[[add_user]]: {}".format(user))
+        if debug: print("[[add_user]]: {}".format(user))
         self.users.append(user)
 
     def get_groups(self):
@@ -35,57 +32,54 @@ class Group(object):
 
     
 def is_user_in_group(user, group, grp_queue = None):
-    #print("[[is_user_in_group]]: group name: {}".format(group.get_name()))
-    #print("[[is_user_in_group]]: group.get_users: {}".format(group.get_users()))
 
     group_queue = []
+
     # Match user to group's name
     if user == group.get_name():
         # print("user matches group name")
         return True
+    
     # Match user to get_users
     elif user in group.get_users():
         # print("user is in group's user list")
         return True
+    
     # if Group's group list not empty, recurse 
     if len(group.get_groups()) == 0:
         # print("group's group list is empty, return False")
         return False
+    
     else:
+        
         # print("group.name = {}".format(group.get_name()))
         # print("Descend into Group's group at a time, group_list: {}".format(group.get_groups()))
         if grp_queue is None:
             grp_queue = group.get_groups()
             return is_user_in_group(user, group, grp_queue)
+
         else:
             grp_queue += group.get_groups()  
             return is_user_in_group(user, grp_queue.pop(0), grp_queue)
 
-
-        ###
-        #grpList = group.get_groups()
-        #i = 1
-        #grp = grpList[:-i]
-        #return is_user_in_group(user, grp))
-        #i += 1
-        
                   
 parent = Group("parent")
 child = Group("child")
 sub_child = Group("subchild")
 
+print("Creating Group Tree")
 sub_child_user = "sub_child_user"
-print("Adding user _sub_child_user_ to sub_child")
+print(" * Adding user _sub_child_user_ to sub_child")
 sub_child.add_user(sub_child_user)
 
-print("Adding group __sub_child__ to child")
+print(" * Adding group __sub_child__ to child")
 child.add_group(sub_child)
 
-print("Adding group __child__ to parent")
+print(" * Adding group __child__ to parent")
 parent.add_group(child)
 
-print("parent's groups: {}".format(parent.get_groups()))
-print("child's name = {}".format(child.get_name()))
+#print("parent's groups: {}".format(parent.get_groups()))
+#print("child's name = {}".format(child.get_name()))
 
 # TEST CASE 1
 print("Is child in group parent? {}".format(is_user_in_group("child", parent)))
